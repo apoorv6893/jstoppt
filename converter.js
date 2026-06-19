91c9d754177ff584b47d2c59a4e32a2840a4f146
apoorv6893 <42558683+apoorv6893@users.noreply.github.com>
2026-06-19 22:50:14 +0530

    Refactor converter.js to use updated PptxGenJS syntax

--- converter.js
+++ converter.js
@@ -1,64 +1,48 @@
-console.log("=== NEW CONVERTER LOADED ===");
-
 const fs = require("fs");
 const PptxGenJS = require("pptxgenjs");
 
 async function run() {
+
     const inputFile = process.argv[2];
     const outputFile = process.argv[3];
 
     let code = fs.readFileSync(inputFile, "utf8");
 
-    // Remove common lines users paste from full examples
     code = code.replace(
-        /const\s+pptx\s*=\s*new\s+PptxGenJS\s*\(\s*\)\s*;?/g,
+        /const\s+pptxgen\s*=\s*require\(["']pptxgenjs["']\);?/g,
         ""
     );
 
     code = code.replace(
-        /let\s+pptx\s*=\s*new\s+PptxGenJS\s*\(\s*\)\s*;?/g,
-        ""
+        /const\s+pres\s*=\s*new\s+pptxgen\(\);?/g,
+        "const pres = new PptxGenJS();"
     );
 
     code = code.replace(
-        /await\s+pptx\.writeFile\s*\([^)]*\)\s*;?/g,
+        /pres\.writeFile\s*\(\s*\{[\s\S]*?\}\s*\)\s*;?/g,
         ""
     );
 
-    code = code.replace(
-        /pptx\.writeFile\s*\([^)]*\)\s*;?/g,
-        ""
-    );
+    const AsyncFunction =
+        Object.getPrototypeOf(async function(){}).constructor;
 
-    const wrappedCode = `
-        const pptx = new PptxGenJS();
-
+    const wrapped = `
         ${code}
 
-        await pptx.writeFile({
+        await pres.writeFile({
             fileName: "${outputFile}"
         });
     `;
 
-    console.log("===== EXECUTING CODE =====");
-    console.log(wrappedCode);
-
-    const AsyncFunction = Object.getPrototypeOf(
-        async function () {}
-    ).constructor;
-
     const fn = new AsyncFunction(
         "PptxGenJS",
-        wrappedCode
+        wrapped
     );
 
     await fn(PptxGenJS);
-
-    console.log("PPT generated successfully");
 }
 
 run().catch(err => {
-    console.error("===== ERROR =====");
     console.error(err);
     process.exit(1);
 });
