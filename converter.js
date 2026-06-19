b01b079ad887026f1e820d954be6c8a05f085f09
apoorv6893 <42558683+apoorv6893@users.noreply.github.com>
2026-06-19 22:51:36 +0530

    Remove PptxGenJS dependencies and related code
    Refactor code to remove PptxGenJS imports and presentation creation logic.

--- converter.js
+++ converter.js
@@ -2,44 +2,66 @@ const fs = require("fs");
 const PptxGenJS = require("pptxgenjs");
 
 async function run() {
-
     const inputFile = process.argv[2];
     const outputFile = process.argv[3];
 
     let code = fs.readFileSync(inputFile, "utf8");
 
+    // Remove imports
     code = code.replace(
         /const\s+pptxgen\s*=\s*require\(["']pptxgenjs["']\);?/g,
         ""
     );
 
     code = code.replace(
-        /const\s+pres\s*=\s*new\s+pptxgen\(\);?/g,
-        "const pres = new PptxGenJS();"
-    );
-
-    code = code.replace(
-        /pres\.writeFile\s*\(\s*\{[\s\S]*?\}\s*\)\s*;?/g,
+        /const\s+PptxGenJS\s*=\s*require\(["']pptxgenjs["']\);?/g,
         ""
     );
 
-    const AsyncFunction =
-        Object.getPrototypeOf(async function(){}).constructor;
+    // Remove presentation creation
+    code = code.replace(
+        /const\s+pres\s*=\s*new\s+pptxgen\s*\(\s*\)\s*;?/g,
+        ""
+    );
 
-    const wrapped = `
+    code = code.replace(
+        /const\s+pptx\s*=\s*new\s+PptxGenJS\s*\(\s*\)\s*;?/g,
+        ""
+    );
+
+    // Remove writeFile calls
+    code = code.replace(
+        /await\s+.*?writeFile\s*\([^)]*\)\s*;?/gs,
+        ""
+    );
+
+    code = code.replace(
+        /.*?writeFile\s*\([^)]*\)\s*;?/gs,
+        ""
+    );
+
+    const wrappedCode = `
+        const pptx = new PptxGenJS();
+        const pres = pptx;
+
         ${code}
 
-        await pres.writeFile({
+        await pptx.writeFile({
             fileName: "${outputFile}"
         });
     `;
 
+    const AsyncFunction =
+        Object.getPrototypeOf(async function(){}).constructor;
+
     const fn = new AsyncFunction(
         "PptxGenJS",
-        wrapped
+        wrappedCode
     );
 
     await fn(PptxGenJS);
+
+    console.log("PPT generated successfully");
 }
 
 run().catch(err => {
